782e2518f471a865c1e4ab12bed3918abf36b88d
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-06-18 13:15:23 +0300

    правки

---
+++ task/CDate.cpp
@@ -0,0 +1,32 @@
+﻿#include "CDate.h"
+
+// Конструкторы:
+// Конструктор по умолчанию (задает начальное значение).
+CDate::CDate()
+{
+	m_day = 1;
+	m_month = 1;
+	m_year = 1;
+}
+
+// Метод определяет на основании даты день недели. // где-то уже делали
+const char* CDate::DayOfWeek() const
+{
+	/*
+	
+	// Функция определяет день недели.
+int Get_DOW(int year, int month, int day)
+{
+	int a = (14 - month) / 12;
+	int y = year - a;
+	int m = month + 12 * a - 2;
+	int d = (7000 + (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12)) % 7;
+
+	return d ? d : 7;
+}
+	
+	*/
+
+	// возвращать нужно в виде строки char*
+	return nullptr;
+}
---
+++ task/CDate.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+class CDate
+{
+private:
+	// Данные - члены(поля) :
+	int m_day;
+	int m_month;
+	int m_year;
+public:
+	// Конструкторы:
+	// Конструктор по умолчанию (задает начальное значение).
+	CDate();
+
+	// Конструктор с всеми параметрами.
+	CDate(int day, int month, int year)
+		:m_day(day), m_month(month), m_year(year) {};
+
+
+	// Метод определяет на основании даты день недели. 
+	const char* DayOfWeek() const;
+
+
+};
+
